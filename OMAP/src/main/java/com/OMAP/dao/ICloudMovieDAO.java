e42a7c8c217b87330e8ca2fb1649669ab7ced6b5
Pranav M <pranavm7@outlook.com>
2022-11-29 07:41:25 -0500

    Updates to ICloudMovieDAO.java

--- OMAP/src/main/java/com/OMAP/dao/ICloudMovieDAO.java
+++ OMAP/src/main/java/com/OMAP/dao/ICloudMovieDAO.java
@@ -2,9 +2,8 @@ package com.OMAP.dao;
 
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
 import com.OMAP.dto.Movie;
 
+
 public interface ICloudMovieDAO extends CrudRepository<Movie, Long> {
-    List<Movie> getMovies() throws Exception;
 }
