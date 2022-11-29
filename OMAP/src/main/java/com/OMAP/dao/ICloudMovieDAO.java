31c009a31908ba9ecd507771cb34e1d30ca86bf4
Pranav M <pranavm7@outlook.com>
2022-11-29 05:32:23 -0500

    Created interface for Azure

--- OMAP/src/main/java/com/OMAP/dao/ICloudMovieDAO.java
+++ OMAP/src/main/java/com/OMAP/dao/ICloudMovieDAO.java
@@ -1,5 +1,10 @@
 package com.OMAP.dao;
 
-public class ICloudMovieDAO {
-    
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import com.OMAP.dto.Movie;
+
+public interface ICloudMovieDAO extends CrudRepository<Movie, Long> {
+    List<Movie> getMovies() throws Exception;
 }
