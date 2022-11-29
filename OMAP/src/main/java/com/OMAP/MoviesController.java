e08b5bcd0ca6492beb58da2d87d0e84e3e1e32c7
Pranav M <pranavm7@outlook.com>
2022-11-29 12:16:19 -0500

    Updated to JpaRepository

--- OMAP/src/main/java/com/OMAP/MoviesController.java
+++ OMAP/src/main/java/com/OMAP/MoviesController.java
@@ -3,10 +3,14 @@ package com.OMAP;
 import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -63,6 +67,16 @@ public class MoviesController {
 		catch(IllegalArgumentException e){
 			return "error";
 		}
+		return "movie-details";
+	}
+
+	@PostMapping("/addMovie")
+	public String addUser(@Validated Movie movie, BindingResult result, Model model){
+		if (result.hasErrors()) {
+            return "add-movie";
+        }
+		cloudMovieRepo.save(movie);
+        model.addAttribute("selectedMovie", movie);
 		return "movie-details";
 	}
 	
